5e76c8f5a21214e98b10c814e66b3122d739d8b2
Dmitry <killer_code@bk.ru>
2021-08-02 19:00:25 +0400

    last fix

--- modules/generator.js
+++
@@ -1,9 +0,0 @@
-const generator = function () {
-  this.nuxt.hook('generate:done', async (context) => {
-    const allRoutes = await Array.from(context.generatedRoutes)
-    const routes = await allRoutes
-    this.nuxt.options.sitemap.routes = await [...routes]
-  })
-}
-
-export default generator
